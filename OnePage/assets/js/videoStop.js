d267c70cb025d5bf20052857ffacca8f8dc17e4b
ericLavachery <eric.lavachery@gmail.com>
2018-06-19 14:22:52 +0200

    les vidéos la clouent quand on ferme le modal

---
+++ OnePage/assets/js/videoStop.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+    /* Get iframe src attribute value i.e. YouTube video url
+    and store it in a variable */
+    var urlStand = $("#vidStand").attr('src');
+    /* Assign empty url value to the iframe src attribute when
+    modal hide, which stop the video playing */
+    $("#modalStand").on('hide.bs.modal', function(){
+        $("#vidStand").attr('src', '');
+    });
+    /* Assign the initially stored url back to the iframe src
+    attribute when modal is displayed again */
+    $("#modalStand").on('show.bs.modal', function(){
+        $("#vidStand").attr('src', urlStand);
+    });
+
+    // idem pour Pi
+    var urlPi = $("#vidPi").attr('src');
+    $("#modalPi").on('hide.bs.modal', function(){
+        $("#vidPi").attr('src', '');
+    });
+    $("#modalPi").on('show.bs.modal', function(){
+        $("#vidPi").attr('src', urlPi);
+    });
+
+    // idem pour Blood
+    var urlBlood = $("#vidBlood").attr('src');
+    $("#modalBlood").on('hide.bs.modal', function(){
+        $("#vidBlood").attr('src', '');
+    });
+    $("#modalBlood").on('show.bs.modal', function(){
+        $("#vidBlood").attr('src', urlBlood);
+    });
+
+    // idem pour Borat
+    var urlBorat = $("#vidBorat").attr('src');
+    $("#modalBorat").on('hide.bs.modal', function(){
+        $("#vidBorat").attr('src', '');
+    });
+    $("#modalBorat").on('show.bs.modal', function(){
+        $("#vidBorat").attr('src', urlBorat);
+    });
+
+    // idem pour Mother
+    var urlMother = $("#vidMother").attr('src');
+    $("#modalMother").on('hide.bs.modal', function(){
+        $("#vidMother").attr('src', '');
+    });
+    $("#modalMother").on('show.bs.modal', function(){
+        $("#vidMother").attr('src', urlMother);
+    });
+});
