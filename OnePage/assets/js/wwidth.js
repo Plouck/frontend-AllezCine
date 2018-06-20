851ec39e3ee9b6c58e83ca6773359e8b2b245fd4
ericLavachery <eric.lavachery@gmail.com>
2018-06-20 11:14:59 +0200

    responsive navbar

---
+++ OnePage/assets/js/wwidth.js
@@ -0,0 +1,18 @@
+function getWindowWidth() {
+ var windowWidth=0;
+ if (typeof(window.innerWidth)=='number') {
+  windowWidth=window.innerWidth;
+    } else {
+  if (document.documentElement && document.documentElement.clientWidth) {
+   windowWidth = document.documentElement.clientWidth;
+        } else {
+   if (document.body&&document.body.clientWidth) {
+    windowWidth=document.body.clientWidth;
+            }
+        }
+    }
+ return windowWidth;
+}
+
+var ww = getWindowWidth();
+console.log(ww);
